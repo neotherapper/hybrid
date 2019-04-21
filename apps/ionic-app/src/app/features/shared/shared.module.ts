fe2c9f8a05cf82f29f0f06cacb7a8cab308fbfcd
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-21 14:38:24 +0100

    translate: fixed issue with translate pipe

--- apps/ionic-app/src/app/features/shared/shared.module.ts
+++ apps/ionic-app/src/app/features/shared/shared.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 
 // xplat
 import { UIModule } from '@workspace/ionic';
+import { TranslateModule } from '@ngx-translate/core';
 
-const MODULES = [UIModule];
+const MODULES = [
+  TranslateModule,
+  UIModule
+];
 
 @NgModule({
   imports: [...MODULES],
