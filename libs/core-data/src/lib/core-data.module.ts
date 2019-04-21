7083c8e4629a43f41badc9ec78d5d34f18b5a6c7
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-21 17:51:23 +0100

    core-data: importing firebase module at core-data module that will be used for fetching data

--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersService } from './users/users.service';
+import { FirebaseModule } from '@workspace/firebase/src';
+
+const MODULES = [
+  CommonModule,
+  FirebaseModule
+];
+
+const PROVIDERS = [
+  UsersService
+]
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    ...MODULES
+  ],
+  providers: [
+    ...PROVIDERS
+  ]
 })
 export class CoreDataModule {}
