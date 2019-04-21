1a7410fb101ed186cb9aeb7c844b241ae332f3a3
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-21 15:17:24 +0100

    firebase: added firebase libraries to lib firebase module

--- libs/firebase/src/lib/firebase.module.ts
+++ libs/firebase/src/lib/firebase.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { environment } from '@workspace/core';
+
+// @angular/fire/ Modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+
+const MODULES = [
+  AngularFirestoreModule,
+  AngularFireStorageModule,
+  AngularFireAuthModule,
+  AngularFireFunctionsModule
+];
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase, 'starwars'),
+    ...MODULES
+  ],
+  exports: [...MODULES]
 })
-export class FirebaseModule {}
+export class FirebaseModule { }
