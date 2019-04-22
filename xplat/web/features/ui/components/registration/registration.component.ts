7364c272c5603b2df838fa169f928d8feb93a6d8
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 23:46:15 +0100

    registration: had to fix bug not implementing abstract class onFacebookSign

--- xplat/web/features/ui/components/registration/registration.component.ts
+++ xplat/web/features/ui/components/registration/registration.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
 import { RegistrationBaseComponent } from '@workspace/features';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'abc-registration',
   templateUrl: 'registration.component.html'
 })
 export class RegistrationComponent extends RegistrationBaseComponent {
-  constructor() {
-    super();
+  constructor(public toastController: ToastController) {
+    super(toastController);
+  }
+
+  onFacebookSign(): void {
+    //here we will call registration dispatcher to sign up the user
+    const toastMessage = 'Facebook sign up handled on xplat web registration';
+    this.message(toastMessage);
   }
 }
