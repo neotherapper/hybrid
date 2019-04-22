dcb113ddfab1ae2c84384310a3984f0c35dba5e6
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 01:34:17 +0100

    starwars-people: using ngrx entity in the reducer

--- libs/core-data/src/lib/state/people/people.reducer.ts
+++ libs/core-data/src/lib/state/people/people.reducer.ts
@@ -1,4 +1,5 @@
 import { PeopleAction, PeopleActionTypes } from './people.actions';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import { Person } from '../../people/person.model';
 
 export const PEOPLE_FEATURE_KEY = 'people';
@@ -10,22 +11,23 @@ export const PEOPLE_FEATURE_KEY = 'people';
  *
  *  Note: replace if already defined in another module
  */
-
-export interface PeopleState {
+export interface PeopleState extends EntityState<Person> {
   list: Person[]; // list of People; analogous to a sql normalized table
   selectedId?: string | number; // which People record has been selected
   loaded: boolean; // has the People list been loaded
   error?: any; // last none error (if any)
 }
 
+export const adapter: EntityAdapter<Person> = createEntityAdapter<Person>();
+
 export interface PeoplePartialState {
   readonly [PEOPLE_FEATURE_KEY]: PeopleState;
 }
 
-export const initialState: PeopleState = {
+export const initialState: PeopleState = adapter.getInitialState({
   list: [],
   loaded: false
-};
+})
 
 export function peopleReducer(
   state: PeopleState = initialState,
@@ -38,8 +40,13 @@ export function peopleReducer(
         list: action.payload,
         loaded: true
       };
+      // not sure if it works
+      // adapter.addAll(action.payload, state);
       break;
     }
   }
   return state;
 }
+
+// get the selectors
+
