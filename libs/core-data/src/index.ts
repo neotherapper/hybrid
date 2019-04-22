a4d14b49c7fdbf3b5154468437e0289309005d63
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 23:54:05 +0100

    trivial changes

--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -2,4 +2,5 @@ export { CoreDataModule } from './lib/core-data.module';
 export { StateModule } from './lib/state/state.module'
 export { PeopleService } from './lib/people/people.service';
 export { PeopleFacade } from './lib/state/people/people.facade';
+export { UserFacade } from './lib/state/user/user.facade';
 export { Person, PersonI } from './lib/people/person.model';
