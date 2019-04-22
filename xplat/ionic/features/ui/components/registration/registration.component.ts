219b113db80e28d0694ff59225b3d3733c1948fc
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 21:29:28 +0100

    added scss file for the ionic registration

--- xplat/ionic/features/ui/components/registration/registration.component.ts
+++ xplat/ionic/features/ui/components/registration/registration.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
 import { RegistrationBaseComponent } from '@workspace/features';
 
 @Component({
-  selector: 'abc-registration',
-  templateUrl: 'registration.component.html'
+  selector: 'abc-ion-registration',
+  templateUrl: 'registration.component.html',
+  styleUrls: ['registration.component.scss']
 })
-export class RegistrationComponent extends RegistrationBaseComponent {
+export class RegistrationComponent extends RegistrationBaseComponent{
+
   constructor() {
     super();
   }
