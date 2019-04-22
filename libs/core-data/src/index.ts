f3cf0eecc2da16bfc067342fdc8d585afe581f63
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 21:14:37 +0100

    added global icon-color and minor changes

--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -1,3 +1,5 @@
 export { CoreDataModule } from './lib/core-data.module';
+export { StateModule } from './lib/state/state.module'
 export { PeopleService } from './lib/people/people.service';
+export { PeopleFacade } from './lib/state/people/people.facade';
 export { Person, PersonI } from './lib/people/person.model';
