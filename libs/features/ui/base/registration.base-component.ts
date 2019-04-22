e1dc58e31ced04fd47f8253744b95d84f57d92ea
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 21:27:49 +0100

    registration: added onSignUp function and registration and submitted variables

--- libs/features/ui/base/registration.base-component.ts
+++ libs/features/ui/base/registration.base-component.ts
@@ -1,9 +1,21 @@
 import { BaseComponent } from '@workspace/core';
+import { Observable } from 'rxjs';
 
 export abstract class RegistrationBaseComponent extends BaseComponent {
-  public text: string = 'Registration';
+  registration = {
+    email: '',
+    password: ''
+  }
+  submitted = false;
+
 
   constructor() {
     super();
+  }
+
+  onSignUp(): void {
+    //here we will call registration dispatcher to sign up the user
+    console.log('%cRegistrationBaseComponent onSignUp', 'color:brown', this.registration );
+    this.submitted = true;
   }
 }
