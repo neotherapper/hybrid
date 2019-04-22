c02fb80b13a2a9b8c8a3bbe3354423a903214f40
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-22 22:49:25 +0100

    registration: added registration page to the ionic app

--- apps/ionic-app/src/app/app.module.ts
+++ apps/ionic-app/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './features/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationModule } from './pages/registration/registration.module';
 
 @NgModule({
-  imports: [CoreModule, SharedModule, AppRoutingModule],
+  imports: [CoreModule, SharedModule, AppRoutingModule, RegistrationModule],
   declarations: [AppComponent],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
---
+++ apps/ionic-app/src/app/pages/registration/registration.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent {};
