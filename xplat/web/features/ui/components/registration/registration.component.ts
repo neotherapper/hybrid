c86e54f79a963617a5b3399b4e488562a07aca9b
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-23 14:10:53 +0100

    fixing issue with registration constructor

--- xplat/web/features/ui/components/registration/registration.component.ts
+++ xplat/web/features/ui/components/registration/registration.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 import { RegistrationBaseComponent } from '@workspace/features';
 import { ToastController } from '@ionic/angular';
+import { UserFacade } from '@workspace/core-data/src';
 
 @Component({
   selector: 'abc-registration',
   templateUrl: 'registration.component.html'
 })
 export class RegistrationComponent extends RegistrationBaseComponent {
-  constructor(public toastController: ToastController) {
-    super(toastController);
+  constructor(public toastController: ToastController, public userFacade: UserFacade) {
+    super(toastController, userFacade);
   }
 
   onFacebookSign(): void {
